68ccec1455ab0a2259091611c5dc3226582c8107
nur-rohmad <nurrohmad776@gmail.com>
2021-05-10 07:49:24 +0700

    perubahan

--- assets/view/header.php
+++ assets/view/header.php
@@ -1,14 +1,47 @@
 <header>
-  <div class="container">
+  <!-- <div class="container">
     <div class="header-left">
       <h1>Pecel Madiun</h1>
     </div>
     <!-- Tambahkan ikon menu disini -->
-    <!-- <span class="fa fa-bars menu-icon" id="icon-menu"></span> -->
+  <!-- <span class="fa fa-bars menu-icon icon-navbar" id="icon-menu"></span>
     <div class="header-right">
       <a href="index.php">Home</a>
       <a href="menu1.php">Menu</a>
       <a href="contac.php"> Contact </a>
     </div>
   </div>
+  <div class="menuHp">
+    <ul>
+      <li><a href="index.php">Home</a></li>
+      <li><a href="menu1.php">Menu</a></li>
+      <li><a href="contac.php"> Contact </a></li>
+    </ul>
+  </div> -->
+
+  <nav class="navbar navbar-expand-lg navbar-dark" style="background-color:  rgba(192, 97, 85, 0.9);">
+    <div class="container">
+      <a class="navbar-brand" href="#">Pecel Madiun</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav ms-auto">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="menu1.php">Menu</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="contac.php">Contact</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </nav>
+
+  <style>
+
+  </style>
 </header>
